82165c3c4f7fae7fa04c93f8520e797a3de37a4b
tyange <usun16@gmail.com>
2023-12-23 22:16:16 +0900

    add submitting indicator

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,7 @@
 import { shareMeal } from "@/lib/actions";
+
 import ImagePicker from "@/components/meals/image-picker";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 import classes from "./page.module.css";
 
@@ -43,7 +45,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
