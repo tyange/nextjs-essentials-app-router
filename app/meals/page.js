c89f262caae744443e8557d42517f510252915a5
tyange <usun16@gmail.com>
2023-12-23 19:05:05 +0900

    error handling

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Link from "next/link";
 import { getMeals } from "@/lib/meals";
 
@@ -5,9 +6,13 @@ import MealsGrid from "@/components/meals/meals-grid";
 
 import classes from "./page.module.css";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -23,7 +28,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
